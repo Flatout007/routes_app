56013728333388c7bf7d7c36b1b06b096efd19b8
Nightwatch <rdmc0072@yahoo.com>
2021-02-24 15:20:12 -0800

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,28 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # resources :users
+
+   #:index
+   get '/users', to: 'users#index', as: 'user'
+
+   #:create
+   post '/users', to: 'user#create', as: 'create_user'
+
+   #new
+   get 'users/new', to: 'user#new', as: 'new_user'
+
+   #edit
+   get 'users/:id/edit', to: 'user#edit', as: 'edit_user'
+
+   #show
+   get 'users/:id', to: 'users#show', as: 'show_user'
+
+   #update
+   patch 'users/:id', to: 'users#update', as: 'update_user'
+
+   #update
+   put 'users/:id', to: 'users#update', as: 'put_update_user'
+
+   #destroy
+   delete 'users/:id', to: 'users#destroy', as: 'delete_user'
 end
