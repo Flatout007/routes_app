72ac7b05b8a72082448416ec8ae0436611d7f04b
Nightwatch <rdmc0072@yahoo.com>
2021-02-25 17:01:08 -0800

    created validations

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ group :development do
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'rails-pry'
+  gem 'annotate'
 end
 
 group :test do
---
+++ app/models/artwork_share.rb
@@ -0,0 +1,9 @@
+# == Schema Information
+#
+# Table name: artwork_shares
+#
+#  id :bigint           not null, primary key
+#
+class ArtworkShare < ApplicationRecord
+
+end
